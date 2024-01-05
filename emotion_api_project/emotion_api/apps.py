1472a1dfb22b5fb534e6c76c0643c56cc84a2398
FotieMConstant <fotiemoghommahieconstant@gmail.com>
2024-01-05 15:14:52 +0300

    done setting up the flask api

--- emotion_api_project/emotion_api/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EmotionApiConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "emotion_api"
--- emotion_api_project/emotion_api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import predict_emotion
-
-urlpatterns = [
-    path('predict_emotion/', predict_emotion, name='predict_emotion'),
-]
